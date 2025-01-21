5b5e330c0f755f7728cd1ef0abc0a71f6ed37460
Ayandip paul <152761756+Ayandip13@users.noreply.github.com>
2025-01-22 00:59:45 +0530

    add some styles

--- todoz/src/screen/TodoScreen.js
+++ todoz/src/screen/TodoScreen.js
@@ -7,18 +7,41 @@ import {
   FlatList,
 } from "react-native";
 import React from "react";
+import {iconButton} from 'react-native-paper' 
+
+const dummyData = [
+  {
+    id: 1,
+    title: "Read a Book",
+  },
+  {
+    id: 2,
+    title: "wakeup at 7 AM",
+  },
+];
 
 const TodoScreen = () => {
-  const dummyData = [
-    {
-      id: 1,
-      title: "Read a Book",
-    },
-    {
-      id: 2,
-      title: "wakeup at 7 AM",
-    },
-  ];
+
+  // items that we're gonna render inside flatlist to show 
+
+  const renderTodos = ({ item }) => {
+    return (
+      <View
+        style={{
+          backgroundColor: "#1e90ff",
+          borderRadius: 8,
+          marginBottom: 12,
+          paddingVertical: 12,
+          paddingHorizontal: 8,
+        }}
+      >
+        <Text style={{ color: "white", textAlign:'center', fontSize:20, fontWeight:400}}>
+          {item.title}
+        </Text>
+      </View>
+    );
+  };
+
 
   return (
     <SafeAreaView style={{ marginHorizontal: 15 }}>
@@ -27,6 +50,7 @@ const TodoScreen = () => {
           borderWidth: 2,
           borderColor: "#1e90ff",
           borderRadius: 6,
+          marginTop: 60,
           paddingVertical: 12,
           paddingHorizontal: 16,
         }}
@@ -37,21 +61,17 @@ const TodoScreen = () => {
           backgroundColor: "black",
           borderRadius: 6,
           paddingVertical: 8,
-          marginTop: 24,
+          marginVertical: 30,
           alignItems: "center",
         }}
+        activeOpacity={0.7}
       >
         <Text style={{ color: "white", fontWeight: "bold", fontSize: 20 }}>
           Add
         </Text>
       </TouchableOpacity>
 
-        <FlatList
-        data={dummyData}
-        renderItem={renderTodos}
-        />
-
-
+      <FlatList data={dummyData} renderItem={renderTodos} />
     </SafeAreaView>
   );
 };
