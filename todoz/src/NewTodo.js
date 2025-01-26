55b4a40d1c1454161388136863e64cfec28710a0
Ayandip paul <152761756+Ayandip13@users.noreply.github.com>
2025-01-27 02:25:04 +0530

    add styles and basic functionality

--- todoz/src/NewTodo.js
+++ todoz/src/NewTodo.js
@@ -8,22 +8,26 @@ import {
   View,
 } from "react-native";
 import React, { useState } from "react";
+import { IconButton } from "react-native-paper";
 
 const NewTodo = () => {
   const [todo, setTodo] = useState("");
   const [todoList, setTodoList] = useState([]);
 
   const addTodo = () => {
+    if (todo === "") return
     setTodoList([...todoList, { id: Date.now().toString(), title: todo }]);
     setTodo("");
   };
 
-  const renderTodos = ({item}) => {
-    return(
-    <View style={styles.todoCard}>
-      <Text style={styles.todoText}>{item.title}</Text>
-    </View>
-    )
+  const renderTodos = ({ item }) => {
+    return (
+      <View style={styles.todoCard}>
+        <IconButton icon="pencil" iconColor="#929AAB" />
+        <Text style={styles.todoText}>{item.title}</Text>
+        <IconButton icon="trash-can" iconColor="#929AAB" />
+      </View>
+    );
   };
 
   return (
@@ -50,17 +54,23 @@ export default NewTodo;
 
 const styles = StyleSheet.create({
   input: {
-    backgroundColor: "#FBFBFB",
+    backgroundColor: "#EEEEEE",
     borderWidth: 1,
     borderColor: "black",
     textAlign: "center",
     borderRadius: 5,
+    borderWidth:0.5,
     color: "black",
+    borderColor: "#393E46",
     marginTop: 30,
     marginBottom: 10,
+    elevation: 8,
   },
   addButton: {
-    backgroundColor: "#B7E0FF",
+    backgroundColor: "#929AAB",
+    elevation: 20,
+    borderWidth: 2,
+    borderColor: "#F1F1F1",
     marginTop: 10,
     paddingVertical: 10,
     borderRadius: 10,
@@ -68,5 +78,24 @@ const styles = StyleSheet.create({
   },
   buttonText: {
     textAlign: "center",
+  },
+  todoCard: {
+    marginTop: 50,
+    backgroundColor: "#EEEEEE",
+    height: 50,
+    width: 285,
+    justifyContent: "center",
+    borderRadius: 9,
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexDirection: "row",
+    paddingHorizontal: 20,
+    marginLeft: 10,
+    elevation: 10,
+    marginBottom: 10,
+    borderWidth:0.1
+  },
+  todoText: {
+    color: "#393E46",
   },
 });
